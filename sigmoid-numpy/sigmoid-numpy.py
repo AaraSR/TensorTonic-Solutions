2900845c11e823d15df5d5f9e30a63a076d0876c
Aaradhya Raut <aaradhya1402@gmail.com>
2026-09-23 12:06:16 +0530

    Solve sigmoid-numpy

--- sigmoid-numpy/sigmoid-numpy.py
+++ sigmoid-numpy/sigmoid-numpy.py
@@ -1,9 +1,9 @@
 import numpy as np
 
-def sigmoid(x):
+def sigmoid(x: list | float) -> np.ndarray | float:
     """
-    Vectorized sigmoid function.
+    Returns the sigmoid value for a scalar or each element of a list.
     """
-    # Write code here
-    x_np = np.asarray(x, dtype=float)
-    return ((1 + np.exp(-x_np)) ** -1)
+    x_np = np.asarray(x)
+    return (1 + (np.exp(-x_np)))**(-1)
+    
